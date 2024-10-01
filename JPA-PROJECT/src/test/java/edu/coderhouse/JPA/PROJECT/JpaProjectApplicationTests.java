f85e1c88fd6998ff436c2c75bc41f17a0a130b1e
Trupiis <153138494+Trupiis@users.noreply.github.com>
2024-10-01 11:39:09 -0300

    ADD JAVA PROJECT "SPRINGBOOT"
    Trabajando con JPA

---
+++ JPA-PROJECT/src/test/java/edu/coderhouse/JPA/PROJECT/JpaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.coderhouse.JPA.PROJECT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
